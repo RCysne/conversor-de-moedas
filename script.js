bf0cbe050d20f67a620da7168e43c6a25bc3caa8
Ronaldo Cysne <ronaldocysne@gmail.com>
2025-04-08 10:13:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const form = document.querySelector('form');
 const amount = document.getElementById('amount');
 const currency = document.getElementById('currency');
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
+// Cotação das moedas
 const usPrice = 5.67;
 const euroPrice = 6.04;
 const libraPrice = 8.77;
 
 
-// Recebendo o input e validando com regex, e caso tenha algum número, corrigir com o replace
+// Recebendo o input e validando com regex, e caso tenha alguma letra, corrigir com o replace que vai aplicar a regra criada no regex
 amount.addEventListener('input', (event) => {
   const hasCharactersRegex = /\D+/g;
-  amount.value = amount.replace(hasCharactersRegex, "");
-  
-  console.log(amount)
+  amount.value = amount.value.replace(hasCharactersRegex, "");
 })
+
+
+
+
+
 
 // Submissão do formulário com uma condicional e chamada da função
 form.onsubmit = (event) => {
   event.preventDefault();
-  console.log(currency.value);
 
   switch (currency.value) {
       case 'USD':
@@ -31,14 +37,40 @@ form.onsubmit = (event) => {
         convertCurrency(amount.value, libraPrice, '£')
         break
   }
-
-  
 }
+
+
+
+
 
 // Função de conversão da moeda e inserção do símbolo
 function convertCurrency(amount, price, symbol) {
-  // console.log(amount, price, symbol);
-  const result = `${symbol} ${amount*price}`
-  console.log(result)
-  return result
+
+  console.log(amount, price, symbol)
+
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    let total = amount * price
+    console.log(total)
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    result.textContent = `${total} Reais`
+    footer.classList.add("show-result")
+  } catch (error) {
+    
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert('Erro no código')
+  }
+
+}
+
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
 }
